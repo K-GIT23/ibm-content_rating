685b4884a7f1ee52026165c9cc00f09061c70901
K-GIT23 <ksql2010@gmail.com>
2025-05-18 16:43:32 -0400

    added text for like/dislike rating

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -23,7 +23,13 @@ class ContentRating extends Component {
      <>
      <h1>Text Content Rating</h1>
      <div className='content-rating'>
-        <p>Text</p>
+        <p>You get to state your like or dislike of the weather
+        Expect partly sunny skies with a modest chance—around 20%—of showers or even an isolated thunderstorm during the day. 
+        Early morning conditions might be a bit more overcast, but the cloud cover should break up as the day progresses. 
+        High temperatures are forecast to reach between 86°F and 88°F, with winds coming from the west at around 10–15 mph and gusts possibly reaching up to 20 mph. 
+        Evening temperatures are likely to dip into the high 60s. 
+
+        </p>
         <div className='rating-buttons'>
         <button className="like-button" onClick={this.state.handleLike}>
             Like ({this.state.likes})
